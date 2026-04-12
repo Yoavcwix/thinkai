560c8363527988171c5bcc797990d0a9b9568eef
Yoav Cohen <yoavc@wix.com>
2026-04-12 12:31:34 +0300

    Initial commit from Create Wix App

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import wix from "@wix/astro";
+import wixPages from "@wix/astro-pages";
+
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [wix(), wixPages(), react()],
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: false,
+    },
+  }),
+
+  image: {
+    domains: ["static.wixstatic.com"],
+  },
+
+  output: "server",
+});
